89d78fa73943db7ab1630db2a87ac48ab4d867ff
ThanhTB <thanhmp49@gmail.com>
2026-03-24 19:42:25 +0700

    Add files via upload

---
+++ bai2.2.5.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Tiến Thành
+ */
+public class bai225 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double a = sc.nextDouble();
+    double b = sc.nextDouble();
+    double tong = a+b;
+    double hieu = a-b;
+    double tich = a*b;
+    double thuong =a/b;
+    
+    
+        System.out.println("tong " + tong + " hieu " + hieu + " tich " + tich + " thuong " + thuong);
+    
+}
+}
